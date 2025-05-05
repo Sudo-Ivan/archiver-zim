de994f56a4aca4a3c13aa06017a037f7924b22bf
Ivan <Sudo-Ivan@users.noreply.github.com>
2025-05-05 14:26:34 -0500

    v0.3.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="archiver-zim",
-    version="0.3.2",
+    version="0.3.3",
     packages=find_packages(),
     install_requires=[
         "yt-dlp>=2025.4.30",
